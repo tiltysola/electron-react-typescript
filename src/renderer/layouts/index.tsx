1912339a9d4bb193f9165bc2585fd89e3c952d5c
悠木 <youmu.ljl@umeng.com>
2020-10-10 15:58:27 +0800

    Add route func

--- src/renderer/layouts/index.tsx
+++ src/renderer/layouts/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HashRouter, NavLink } from 'react-router-dom'
+import useInterval from '@use-it/interval'
+// import _ from 'lodash'
 
 import Router from './router'
 
 import './style.less'
 
 const Index = () => {
+  const [hash, setHash] = useState<any>(window.location.hash)
+
+  useInterval(() => {
+    setHash(() => window.location.hash)
+  }, 100)
+
   return (
     <HashRouter>
       <div id="layout">
         <div className="layout-header">
           <div className="clearfix" />
-          <span className="slg">Typescript, the best language for f2e!</span>
+          <span className="slg">Now you are at: {hash}</span>
         </div>
         <div className="layout-content">
           <div className="clearfix" />
@@ -20,7 +28,7 @@ const Index = () => {
         <div className="layout-footer">
           <div className="clearfix" />
           <div className="nav">
-            <NavLink to="/">Index</NavLink>
+            <NavLink to="/welcome">Index</NavLink>
             <NavLink to="/about">About</NavLink>
           </div>
         </div>
